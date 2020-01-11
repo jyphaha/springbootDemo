00c82fd196780490cd03f6a5dd7e28c58531d7a7
jyp <964126796@qq.com>
2020-01-11 14:08:28 +0800

    demo

---
+++ src/main/java/com/example/controllor/controller.java
@@ -0,0 +1,15 @@
+package com.example.controllor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class controller {
+
+    @RequestMapping("/boot/hello")  //请求路径
+    public @ResponseBody  String hello(){
+        return "hello the world!";
+    }
+}
